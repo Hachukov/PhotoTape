9a6284cd841847d7b5fee7b48a57d8c3228a4b0a
Арсен Хачуков <hachukov.arsen@gmail.com>
2022-11-23 13:55:39 +0300

    Добавлены цвета + расширение для цветов

---
+++ PhotoTape/Helpers/UIColor+Extentions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extentions.swift
+//  PhotoTape
+//
+//  Created by Arsen Hachuk on 23.11.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var YPBackground: UIColor { UIColor(named: "YP Background")! }
+    static var YPRed: UIColor {UIColor(named: "YP Red")! }
+    static var YPWhite: UIColor {UIColor(named: "YP White")! }
+    static var YPBlack: UIColor {UIColor(named: "YP Black")! }
+    static var YPGray: UIColor {UIColor(named: "YP Gray")! }
+    static var YPBlue: UIColor {UIColor(named: "YP Blue")! }
+    static var YPWhiteAlpha50: UIColor {UIColor(named: "YP White (Alpha 50)")! }
+}
