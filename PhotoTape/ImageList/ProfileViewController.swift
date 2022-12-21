5dfec855f370e0dd02f0cd8cd7b66dd2b37f823a
Арсен Хачуков <hachukov.arsen@gmail.com>
2022-12-21 20:25:52 +0300

    sprint_9

--- PhotoTape/ImageList/ProfileViewController.swift
+++ PhotoTape/ImageList/ProfileViewController.swift
@@ -8,20 +8,52 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
-    @IBOutlet weak var profileImage: UIImageView!
-    
-    @IBOutlet weak var nameLabel: UILabel!
-    
-    @IBOutlet weak var mailLabel: UILabel!
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let profileImage = UIImageView()
+        profileImage.image = UIImage(named: "Photo")
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileImage)
+        profileImage.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImage.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                                              constant: 20).isActive = true
+        profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                          constant: 20).isActive = true
+        
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        nameLabel.heightAnchor.constraint(equalToConstant: 23).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 8).isActive = true
+        nameLabel.textColor = .white
+        
+        let mailLabel = UILabel()
+        mailLabel.text = "@ekaterina_nov"
+        mailLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mailLabel)
+        mailLabel.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        mailLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        mailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        mailLabel.textColor = .ypGray
+        mailLabel.font = UIFont(name: "YS Display-Medium", size: 13)
+        
+        let logoutImag = UIImageView()
+        logoutImag.image = UIImage(systemName: "ipad.and.arrow.forward")
+        logoutImag.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoutImag)
+        logoutImag.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        logoutImag.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        logoutImag.centerYAnchor.constraint(equalTo: profileImage.centerYAnchor).isActive = true
+        logoutImag.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                             constant: -26).isActive = true
+        logoutImag.tintColor = .ypRed
         
     }
-    
-    @IBAction func logoutButton(_ sender: Any) {
-    }
-    
 }
